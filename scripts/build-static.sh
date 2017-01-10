9323c1c95171736a3931799359b61f549e8234a5
Eric van Zanten <eric.vanzanten@gmail.com>
2017-01-10 14:19:51 -0600

    Fix bash thing

--- scripts/build-static.sh
+++ scripts/build-static.sh
@@ -18,7 +18,7 @@ if [ ! -d $source ]; then
     
     hostname=`cat $source/CNAME`
     
-    scripts_dir = `pwd`
+    scripts_dir=`pwd`
     part1=$scripts_dir/nginx_conf_part_1.conf
     part2=$scripts_dir/nginx_conf_part_2.conf
     
